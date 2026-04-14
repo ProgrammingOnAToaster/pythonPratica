2ec08a3d19b6dad06deb874ccf313b7cfb534944
Vitor Almeida <vitorbackenddeveloper@gmail.com>
2026-04-14 00:10:14 -0300

    saudação conforme nome e hora

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,12 @@
-name = str(input("Digite seu nome: "))
+#File that take a name and give up an greeting 
+name = str(input("Send a name: "))
 
-print("Olá, " + name + "!")
+time = int(input("What time is it? "))
+
+if(time >= 00 and time < 12):
+    print("Good Morning, " + name + "!")
+
+else:
+    print("Good Afternoon, " + name + "!")
+
 
