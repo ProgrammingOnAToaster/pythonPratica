bf43f98ef3ce84fd7e271d432f6708f60657d91b
Vitor Almeida <vitorbackenddeveloper@gmail.com>
2026-04-23 23:24:00 -0300

    Adicionado o uso de f-string

--- hello_world.py
+++ hello_world.py
@@ -1,12 +1,13 @@
 #File that take a name and give up an greeting 
+
 name = str(input("Send a name: "))
 
 time = int(input("What time is it? "))
 
 if(time >= 00 and time < 12):
-    print("Good Morning, " + name + "!")
+    print(f"Good Morning,{name} !")
 
 else:
-    print("Good Afternoon, " + name + "!")
+    print(f"Good Afternoon, {name}!")
 
 
