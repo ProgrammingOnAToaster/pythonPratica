537a13ee54534d422fd55b0a08415d2be773c5a7
Vitor Almeida <vitorbackenddeveloper@gmail.com>
2026-04-27 22:03:40 -0300

    new class with vect of names

---
+++ vect_names.py
@@ -0,0 +1,18 @@
+#Generate a vect of names with first letter "a"
+
+quantity_names = int(input("Enter with number of names: "))
+
+vect = []
+
+for i in range(quantity_names):
+    name = input("Enter with a name: ")
+    if name[0] == 'A':
+        vect.append(name)
+
+print('\n')
+print(f'names with A: ')
+for names in vect:
+    print(names)
+
+
+
