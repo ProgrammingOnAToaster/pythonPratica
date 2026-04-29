19023892b8dcfbf5851bcd33b26e9cb5d4050c30
Vitor Almeida <vitorbackenddeveloper@gmail.com>
2026-04-28 22:48:25 -0300

    Exception handling added to the vect class

--- vect_names.py
+++ vect_names.py
@@ -1,18 +1,28 @@
 #Generate a vect of names with first letter "a"
+while True:
+    try:
+        quantity_names = int(input("Enter with number of names: "))
 
-quantity_names = int(input("Enter with number of names: "))
+        vect = []
 
-vect = []
+        for i in range(quantity_names):
+            name = input("Enter with a name: ")
+            if name[0] == 'A':
+                vect.append(name)
 
-for i in range(quantity_names):
-    name = input("Enter with a name: ")
-    if name[0] == 'A':
-        vect.append(name)
+        print('\n')
+        print(f'names with A: ')
+        for names in vect:
+            print(names)
+        
+        break
 
-print('\n')
-print(f'names with A: ')
-for names in vect:
-    print(names)
+    except ValueError:
+        print("Error, please enter an integer number!")
+
+
+
+
 
 
 
